9c26bdb2f64e6a10d70ebfb36b9da88ba124bf43
milindasandaru <sams.senarath@gmail.com>
2026-04-23 16:16:13 +0530

    chore: implement and update util files

--- backend/src/main/java/com/smartcampus/hub/util/ResourceStatus.java
+++ backend/src/main/java/com/smartcampus/hub/util/ResourceStatus.java
@@ -1,6 +1,7 @@
 package com.smartcampus.hub.util;
 
 public enum ResourceStatus {
+    ACTIVE,
     AVAILABLE,
     RESERVED,
     OUT_OF_SERVICE
--- backend/src/main/java/com/smartcampus/hub/util/ResourceType.java
+++ backend/src/main/java/com/smartcampus/hub/util/ResourceType.java
@@ -3,6 +3,7 @@ package com.smartcampus.hub.util;
 public enum ResourceType {
     LECTURE_HALL,
     LAB,
+    STUDY_ROOM,
     BOOK,
     STUDY_AREA,
     DOCUMENT
